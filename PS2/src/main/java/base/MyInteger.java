e80f2c3276fa52b230a0c00e3615911f467e9da0
chimaakp <chimaakp@ChimaII>
2016-03-12 00:26:21 -0500

    lkfv

--- PS2/src/main/java/base/MyInteger.java
+++ PS2/src/main/java/base/MyInteger.java
@@ -3,15 +3,17 @@ package base;
 public class MyInteger {
 	
 	int iValue;
-	
+	//constuctor
 	MyInteger(int iValue){
 		iValue = this.iValue;
 	}
 	
+	//getter
 	public int getiValue(){
 		return iValue;		
 	}
 	
+	//methods
 	public boolean isEven(){
 		int etest = iValue % 2;
 		if(etest == 0){
@@ -42,9 +44,54 @@ public class MyInteger {
 		
 	}
 
+
 	
 	
+	//int methods
+	public static boolean isEven(int e){
+		int etest = e % 2;
+		if(etest == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public static boolean isOdd(int o){
+		int otest = o % 2;
+		if(otest == 1){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}		
 	
 	
+	public static boolean isPrime(int pri) {
+		for(int p = 2; p < (pri / 2) ; p++) {
+			if(pri % p == 0)
+				return false;
+		}
+		return true;
+		
+	}	
+	
+	
+	//MyInteger methods
+	public static boolean isEven(MyInteger eve){
+		return eve.isEven();
+		
+	}
+	
+	public static boolean isOdd(MyInteger odd){
+		return odd.isOdd();
+		
+	}
+	
+	public static boolean isPrime(MyInteger prim){
+		return prim.isPrime();
+	}
 	
 }
